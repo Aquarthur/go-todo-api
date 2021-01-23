d60e3c50505a46b0dd1a65d0c5ba32e1d8083f98
Arthur Verkaik <art.v412@gmail.com>
2021-01-23 15:32:08 -0800

    implement find all and create handlers

--- handlers/todo.go
+++ handlers/todo.go
@@ -1,6 +1,13 @@
 package handlers
 
 import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Aquarthur/go-todo-api/domain"
+
+	"github.com/Aquarthur/go-todo-api/handlers/models"
+
 	"github.com/Aquarthur/go-todo-api/repository"
 )
 
@@ -12,4 +19,45 @@ func NewTodoHandler(todoRepository repository.TodoRepository) *TodoHandler {
 	return &TodoHandler{
 		repository: todoRepository,
 	}
+}
+
+func (todoHandler *TodoHandler) FindAll(w http.ResponseWriter, req *http.Request) {
+	todos, err := todoHandler.repository.FindAllTodos()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	todosBytes, err := json.Marshal(todos)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(todosBytes)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (todoHandler *TodoHandler) Create(w http.ResponseWriter, req *http.Request) {
+	payload := new(models.TodoDTO)
+	err := json.NewDecoder(req.Body).Decode(payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	todo := new(domain.Todo)
+	todo.FromDTO(payload)
+
+	err = todoHandler.repository.SaveTodo(todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	todoBytes, err := json.Marshal(todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(todoBytes)
+	w.WriteHeader(http.StatusCreated)
 }
